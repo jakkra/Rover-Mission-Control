ef89e2f5692fdb8549327b650be6452f739a6fe4
Jakob Krantz <mail@jakobkrantz.se>
2020-06-11 21:35:06 +0200

    Add Rover head flash light control.

--- src/ImageCard.js
+++ src/ImageCard.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 
 import { Button, ButtonGroup, Grid } from 'react-bootstrap';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLightbulb } from '@fortawesome/free-solid-svg-icons'
+
 const styles = {
   container: {
     textAlign: 'center',
@@ -29,7 +32,8 @@ export default class ImageCard extends React.Component {
   static defaultProps = {
     streamAddress: 'http://192.168.4.10:81/stream',
     captureAddress: 'http://192.168.4.10/capture',
-    setQualityAddress: 'http://192.168.4.10/control?var=quality&val=30'
+    setQualityAddress: 'http://192.168.4.10/control?var=quality&val=30',
+    setLedState: 'http://192.168.4.10/control?var=led&val='
   };
 
   constructor(props) {
@@ -37,9 +41,11 @@ export default class ImageCard extends React.Component {
     this.startStream = this.startStream.bind(this);
     this.stopStream = this.stopStream.bind(this);
     this.captureStill = this.captureStill.bind(this);
+    this.toggleLed = this.toggleLed.bind(this);
 
     this.state = {
-      imgSrc: ''
+      imgSrc: '',
+      flashOn: false
     }
   }
 
@@ -70,13 +76,37 @@ export default class ImageCard extends React.Component {
     });
   }
 
+  toggleLed() {
+    let url = this.props.setLedState
+    this.setState({
+      flashOn: !this.state.flashOn
+    });
+    if (this.state.flashOn === false) {
+      url += '1';
+    } else {
+      url += '0';
+    }
+
+    fetch(url)
+    .catch((err) => console.log('Failed setting LED state'));
+  }
+
   render() {
+    let icon;
+
+    if (this.state.flashOn === true) {
+      icon = (<FontAwesomeIcon icon={faLightbulb} style={{ color: 'white', fontSize: '1em'}} />);
+    } else {
+      icon = (<FontAwesomeIcon icon={faLightbulb} style={{ color: 'black', fontSize: '1em'}} />);
+    }
+
     return (
       <Grid style={ styles.container }>
         <ButtonGroup >
-          <Button style={styles.button} bsSize="small" bsStyle="success" onClick={this.startStream}>Start stream</Button>
-          <Button style={styles.button} bsSize="small" bsStyle="danger" onClick={this.stopStream}>Stop stream</Button>
+          <Button style={styles.button} bsSize="small" bsStyle="success" onClick={this.startStream}>Start</Button>
+          <Button style={styles.button} bsSize="small" bsStyle="danger" onClick={this.stopStream}>Stop</Button>
           <Button style={styles.button} bsSize="small" bsStyle="primary" onClick={this.captureStill}>Screenshot</Button>
+          <Button style={{...styles.button, ...{ paddingLeft: 10, paddingRight: 10}}} bsSize="small" bsStyle="danger" onClick={this.toggleLed}>{icon}</Button>
         </ButtonGroup>
         <img style = {styles.image} src={this.state.imgSrc} alt="" />
       </Grid>
